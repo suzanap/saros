944e9d1900215c2172e51c03367e659cffe349a9
Oliver Hanßen <oliver.hanssen@gmail.com>
2018-08-11 14:26:02 +0200

    [INTERNAL][CORE] SharedProjectMapper: core.IProject replaced by core.IReferencePoint

---
+++ de.fu_berlin.inf.dpp.core/src/de/fu_berlin/inf/dpp/session/IReferencePointManager.java
@@ -0,0 +1,25 @@
+package de.fu_berlin.inf.dpp.session;
+
+import de.fu_berlin.inf.dpp.filesystem.IFile;
+import de.fu_berlin.inf.dpp.filesystem.IFolder;
+import de.fu_berlin.inf.dpp.filesystem.IPath;
+import de.fu_berlin.inf.dpp.filesystem.IProject;
+import de.fu_berlin.inf.dpp.filesystem.IReferencePoint;
+import de.fu_berlin.inf.dpp.filesystem.IResource;
+
+public interface IReferencePointManager {
+
+    void put(IReferencePoint referencePoint, IProject project);
+
+    IProject get(IReferencePoint referencePoint);
+
+    IFile getFile(IReferencePoint referencePoint, String relativePath);
+
+    IFile getFile(IReferencePoint referencePoint, IPath relativePath);
+
+    IFolder getFolder(IReferencePoint referencePoint, String relativePath);
+
+    IFolder getFolder(IReferencePoint referencePoint, IPath relativePath);
+
+    IResource findMember(IReferencePoint referencePoint, IPath relativePath);
+}
