6ef89abc708f30cba8c6f4b69f6f7b403d1272cf
Daniel Paul-Gattringer <danielpaul@zedat.fu-berlin.de>
2018-11-28 09:00:14 +0100

    reimplemented EditAccount, GetValidJID, RenameContact, ShowAccountPage and ShowSessionWizard

--- de.fu_berlin.inf.dpp.ui/src/de/fu_berlin/inf/dpp/HTMLUIStrings.kt
+++ de.fu_berlin.inf.dpp.ui/src/de/fu_berlin/inf/dpp/HTMLUIStrings.kt
@@ -9,34 +9,34 @@ package de.fu_berlin.inf.dpp
 object HTMLUIStrings {
 	@JvmStatic
 	// dialog titles
-	val TITLE_MAIN_PAGE: String? = "Main page"
+	val TITLE_MAIN_PAGE: String = "Main page"
 	@JvmStatic
-	val TITLE_START_SESSION_WIZARD: String? = "Share Project"
+	val TITLE_START_SESSION_WIZARD: String = "Share Project"
 	@JvmStatic
-	val TITLE_ADD_ACCOUNT_PAGE: String? = "Add Account"
+	val TITLE_ADD_ACCOUNT_PAGE: String = "Add Account"
 	@JvmStatic
-	val TITLE_CONFIGURATION_PAGE: String? = "Configure Saros"
+	val TITLE_CONFIGURATION_PAGE: String = "Configure Saros"
 	// error messages
 	@JvmStatic
-	val ERR_ACCOUNT_ALREADY_PRESENT: String? = "Couldn't create account. Account already present"
+	val ERR_ACCOUNT_ALREADY_PRESENT: String = "Couldn't create account. Account already present"
 	@JvmStatic
-	val ERR_ACCOUNT_EDIT_FAILED: String? = "Couldn't change the account."
+	val ERR_ACCOUNT_EDIT_FAILED: String = "Couldn't change the account."
 	@JvmStatic
-	val ERR_ACCOUNT_SET_ACTIVE_FAILED: String? = "Error while trying to change the active account."
+	val ERR_ACCOUNT_SET_ACTIVE_FAILED: String = "Error while trying to change the active account."
 	@JvmStatic
-	val ERR_ACCOUNT_DELETE_ACTIVE: String? =
+	val ERR_ACCOUNT_DELETE_ACTIVE: String =
 		"Couldn't delete the account. The currently active account cannot be deleted"
 	@JvmStatic
-	val ERR_CONTACT_INVALID_JID: String? = "The Jabber ID must be in the format user@domain."
+	val ERR_CONTACT_INVALID_JID: String = "The Jabber ID must be in the format user@domain."
 	@JvmStatic
-	val ERR_CONTACT_DELETE_FAILED: String? = "Failed to delete contact."
+	val ERR_CONTACT_DELETE_FAILED: String = "Failed to delete contact."
 	@JvmStatic
-	val ERR_CONTACT_ADD_FAILED: String? = "Failed to add contact."
+	val ERR_CONTACT_ADD_FAILED: String = "Failed to add contact."
 	@JvmStatic
-	val ERR_CONTACT_RENAME_FAILED: String? = "Failed to rename contact."
+	val ERR_CONTACT_RENAME_FAILED: String = "Failed to rename contact."
 	@JvmStatic
-	val ERR_SESSION_START_CANCELED: String? = "Couldn't send session invitaion."
+	val ERR_SESSION_START_CANCELED: String = "Couldn't send session invitaion."
 	@JvmStatic
-	val ERR_SESSION_PROJECT_LIST_IOEXCEPTION: String? =
+	val ERR_SESSION_PROJECT_LIST_IOEXCEPTION: String =
 		"An error occurred while trying to create a list of all files to share."
 }// Hide implicit public constructor
