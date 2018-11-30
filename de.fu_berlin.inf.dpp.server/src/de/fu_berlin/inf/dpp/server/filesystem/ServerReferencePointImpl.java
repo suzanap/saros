f60ece30aae31aaf216adbb43c6efa6e8f6c2a5e
Oliver Hanßen <oliver.hanssen@gmail.com>
2018-11-30 16:56:26 +0100

    [INTERNAL] Added IReferencePoint and its implementation
    The Saros filesystem gets a new component in its filesystem:
    IReferencePoint. The first step of the refactoring that the
    reference point of each files/folders points on the location
    of the projects. So the IResource interface gets a new method
    called getReferencePoint() and returns the reference point.
    The IReferencePoint interface is empty. Its implementation
    contains an IPath object from the location of the referenced
    resource (project at first) and an IReferencePoint object is
    comparable over the IPath object.

---
+++ de.fu_berlin.inf.dpp.server/src/de/fu_berlin/inf/dpp/server/filesystem/ServerReferencePointImpl.java
@@ -0,0 +1,38 @@
+package de.fu_berlin.inf.dpp.server.filesystem;
+
+import de.fu_berlin.inf.dpp.filesystem.IPath;
+import de.fu_berlin.inf.dpp.filesystem.IReferencePoint;
+
+public class ServerReferencePointImpl implements IReferencePoint {
+
+  private final IPath path;
+
+  public ServerReferencePointImpl(IPath path) {
+    this.path = path;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((path == null) ? 0 : path.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (getClass() != obj.getClass()) return false;
+    ServerReferencePointImpl other = (ServerReferencePointImpl) obj;
+    if (path == null) {
+      if (other.path != null) return false;
+    } else if (!path.equals(other.path)) return false;
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return path.toString();
+  }
+}
