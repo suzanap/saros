2663cf462af1ab64c7158090fb66a5a48ea0838f
Christian <chrisjberlin@googlemail.com>
2019-01-26 13:14:07 +0100

    AddGitManager,Test and attributes toGitActivities
    Manager is responsible for negotiation
    GitCollectActivities send Hash
    GitSendBundleActivities send Commits

--- de.fu_berlin.inf.dpp.core/src/de/fu_berlin/inf/dpp/activities/GitSendBundleActivity.java
+++ de.fu_berlin.inf.dpp.core/src/de/fu_berlin/inf/dpp/activities/GitSendBundleActivity.java
@@ -1,17 +1,28 @@
 package de.fu_berlin.inf.dpp.activities;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamAsAttribute;
 import de.fu_berlin.inf.dpp.session.User;
 
-@XStreamAlias("gitSendBundleActivity")
+@XStreamAlias("GitSendBundleActivity")
 public class GitSendBundleActivity extends AbstractActivity {
+  @XStreamAsAttribute byte[] bundle;
 
-  public GitSendBundleActivity(User source) {
+  public GitSendBundleActivity(User source, byte[] bundle) {
     super(source);
+    setBundle(bundle);
   }
 
   @Override
   public void dispatch(IActivityReceiver receiver) {
     receiver.receive(this);
+  }
+
+  private void setBundle(byte[] bundle) {
+    this.bundle = bundle;
+  }
+
+  public byte[] getBundle() {
+    return bundle;
   }
 }
