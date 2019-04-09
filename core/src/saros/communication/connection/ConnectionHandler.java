673fa5156a3e6eb2504d6cd3b17559572b6ed860
Kelvin Glaß <kelvin.glass@fu-berlin.de>
2019-04-09 15:23:25 +0200

    [INTERNAL] Use relocated picocontainer package name

--- core/src/saros/communication/connection/ConnectionHandler.java
+++ core/src/saros/communication/connection/ConnectionHandler.java
@@ -9,7 +9,6 @@ import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smack.proxy.ProxyInfo;
 import org.jivesoftware.smackx.ServiceDiscoveryManager;
-import org.picocontainer.annotations.Nullable;
 import saros.SarosConstants;
 import saros.account.XMPPAccount;
 import saros.net.ConnectionState;
@@ -20,6 +19,7 @@ import saros.net.xmpp.IConnectionListener;
 import saros.net.xmpp.JID;
 import saros.net.xmpp.XMPPConnectionService;
 import saros.preferences.Preferences;
+import saros.repackaged.picocontainer.annotations.Nullable;
 
 /**
  * Facade for handling connection establishment and connection events. This facade should be
