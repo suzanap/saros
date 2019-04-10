58bff4587d69f1fe98967856eb16e48f1eacb2a8
Tobias Bouschen <tobias.bouschen@googlemail.com>
2019-04-10 15:00:38 +0200

    [INTERNAL][I] Implement logic determining the current validity state
    Implement the logic to determine the current validity state of the
    ModuleTab. The logic is modeled after current module restrictions,
    meaning the new/existing module name has to match the remote module
    name.

--- intellij/src/saros/intellij/ui/wizards/pages/moduleselection/ModuleTab.java
+++ intellij/src/saros/intellij/ui/wizards/pages/moduleselection/ModuleTab.java
@@ -16,6 +16,10 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
+import java.io.File;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
@@ -82,11 +86,6 @@ class ModuleTab {
     addProjectComboBoxListener();
     addCreateNewModuleFieldListeners();
     addUseExistingModuleFieldListeners();
-
-    /*
-     * TODO set up logic to determine whether the current input is valid
-     *  - add logic to updateInputValidity
-     */
   }
 
   /**
@@ -333,8 +332,33 @@ class ModuleTab {
    * <p>This method should be called by all change listeners of the panel components.
    */
   private void updateInputValidity() {
-    // TODO check input validity and set correct value
-    boolean newInputValidityState = false;
+    boolean newInputValidityState;
+
+    if (createNewModuleRadioButton.isSelected()) {
+      boolean hasValidNewName = moduleName.equals(newModuleNameTextField.getText());
+
+      boolean hasValidNewBasePath;
+      try {
+        Path newBasePath = Paths.get(newModuleBasePathTextField.getText());
+        File newBasePathFile = newBasePath.toFile();
+
+        hasValidNewBasePath = newBasePathFile.exists() && newBasePathFile.isDirectory();
+
+      } catch (InvalidPathException | UnsupportedOperationException e) {
+        hasValidNewBasePath = false;
+      }
+
+      newInputValidityState = hasValidNewName && hasValidNewBasePath;
+
+    } else if (useExistingModuleRadioButton.isSelected()) {
+      Module selectedExistingModule = existingModuleComboBox.getSelectedEntry();
+
+      newInputValidityState =
+          selectedExistingModule != null && selectedExistingModule.getName().equals(moduleName);
+
+    } else {
+      newInputValidityState = false;
+    }
 
     if (newInputValidityState != hasValidInput) {
       hasValidInput = newInputValidityState;
